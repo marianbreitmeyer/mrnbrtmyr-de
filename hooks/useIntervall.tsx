f31f6da080ba822365145c2ed5944caabec0a8d5
Marian Breitmeyer <info@marianbreitmeyer.de>
2024-04-06 14:18:18 +0200

    fix: Intervall behaviour when out of focus

--- hooks/useIntervall.tsx
+++ hooks/useIntervall.tsx
@@ -1,26 +1,68 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
-export default function useInterval(
-  callback: React.EffectCallback,
-  delay: number | null
-): React.MutableRefObject<number | null> {
-  const intervalRef = useRef<number | null>(null);
-  const callbackRef = useRef(callback);
+const useInterval = (callback: () => void, delay: number) => {
+  const savedCallback = useRef<() => void>();
 
   useEffect(() => {
-    callbackRef.current = callback;
+    savedCallback.current = callback;
   }, [callback]);
 
   useEffect(() => {
-    if (typeof delay === 'number') {
-      intervalRef.current = window.setInterval(
-        () => callbackRef.current(),
-        delay
-      );
+    const handler = () => {
+      if (savedCallback.current) {
+        savedCallback.current();
+      }
+    };
 
-      return () => window.clearInterval(intervalRef.current || 0);
-    }
+    let id: NodeJS.Timeout;
+
+    const onFocus = () => {
+      id = setInterval(handler, delay);
+    };
+
+    const onBlur = () => {
+      clearInterval(id);
+    };
+
+    onFocus();
+
+    window.addEventListener('focus', onFocus);
+    window.addEventListener('blur', onBlur);
+
+    return () => {
+      clearInterval(id);
+      window.removeEventListener('focus', onFocus);
+      window.removeEventListener('blur', onBlur);
+    };
   }, [delay]);
+};
 
-  return intervalRef;
-}
+export default useInterval;
+
+// Old Implementation which causes problems when tab gets out of focus
+// import React, { useEffect, useRef } from 'react';
+
+// export default function useInterval(
+//   callback: React.EffectCallback,
+//   delay: number | null
+// ): React.MutableRefObject<number | null> {
+//   const intervalRef = useRef<number | null>(null);
+//   const callbackRef = useRef(callback);
+
+//   useEffect(() => {
+//     callbackRef.current = callback;
+//   }, [callback]);
+
+//   useEffect(() => {
+//     if (typeof delay === 'number') {
+//       intervalRef.current = window.setInterval(
+//         () => callbackRef.current(),
+//         delay
+//       );
+
+//       return () => window.clearInterval(intervalRef.current || 0);
+//     }
+//   }, [delay]);
+
+//   return intervalRef;
+// }
