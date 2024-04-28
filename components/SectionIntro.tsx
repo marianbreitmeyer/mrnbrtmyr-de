b5eda2faf37cf8d9298361ce51b7ec1850b451e0
Marian Breitmeyer <info@marianbreitmeyer.de>
2024-04-28 14:41:56 +0200

    add: visual changes

--- components/SectionIntro.tsx
+++ components/SectionIntro.tsx
@@ -2,7 +2,7 @@ import VisualGrid from '@/components/VisualGrid';
 
 const SectionIntro = () => (
   <section className="my-14">
-    <div className="wrapper bg-white/40 h-padding">
+    <div className="wrapper bg-white/40 dark:bg-stone-950/60 h-padding">
       <h1 className="text-5xl leading-[3.25rem] font-serif">
         Crafting polished & <i>human-centered</i> software, mobile apps, and web
         experiences for more than a decade.
