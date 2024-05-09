d70b8396823f203c3f9d05235835adaf5e961775
Marian Breitmeyer <info@marianbreitmeyer.de>
2024-05-09 14:51:30 +0200

    fix: styling mobile

--- components/RoleHeader.tsx
+++ components/RoleHeader.tsx
@@ -9,7 +9,12 @@ const RoleHeader = ({
   role: string;
   className?: string;
 }) => (
-  <div className={cn('flex flex-row gap-x-2 md:gap-x-4 text-sm', className)}>
+  <div
+    className={cn(
+      'flex flex-col md:flex-row gap-x-2 md:gap-x-4 text-sm',
+      className
+    )}
+  >
     <h4 className="text-stone-600 dark:stone-text-400">{role}</h4>
     {date && (
       <p className="text-stone-400 dark:stone-text-600 font-mono">{date}</p>
