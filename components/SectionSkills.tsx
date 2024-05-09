931b097fad1cba8b90efe925c8235a7c5b548564
Marian Breitmeyer <info@marianbreitmeyer.de>
2024-05-09 14:12:12 +0200

    rm: old components

--- components/SectionSkills.tsx
+++
@@ -1,9 +0,0 @@
-import Section from '@/components/Section';
-
-const SectionSkills = () => (
-  <Section title="Skills">
-    <p>More Content goes here.</p>
-  </Section>
-);
-
-export default SectionSkills;
