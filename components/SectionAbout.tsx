8f41ca9d008bc9b0557a7bac3b6acda3679098b7
Marian Breitmeyer <info@marianbreitmeyer.de>
2024-05-12 22:34:32 +0200

    fix: text change

--- components/SectionAbout.tsx
+++ components/SectionAbout.tsx
@@ -25,7 +25,8 @@ const PartAbout = () => (
       </div>
       <div className="col-span-8 md:col-span-5 gap-y-6 md:gap-y-10 flex flex-col p-6 md:p-10">
         <TextBlock
-          copy={`I’ve been crafting human-centered software, mobile apps and web experiences for over a decade. I am dedicated to simplifying complexity and designing intuitive systems. Even though I consider myself a designer, I have found code to be the best tool for translating my ideas into reality. I excel on bridging the gap between design and development, ensuring the creation of delightful and ethical solutions to provide value to both users and businesses.
+          copy={`I’ve been crafting human-centered software, mobile apps and web experiences for over a decade. I am dedicated to simplifying complexity and designing intuitive systems. 
+          Although I consider myself a designer, I have found code to be the best tool for translating my ideas into reality. I excel on bridging the gap between design and development, ensuring the creation of delightful and ethical solutions to provide value to both users and businesses.
 
 I have designed user interfaces and experiences, mapped out and implemented roadmaps and features, led cross-functional teams, mentored other designers, and fostered the growth of design teams. All within agency and internal corporate environments.`}
         />
