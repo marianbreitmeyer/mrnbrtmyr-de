2f89e80de6290abdabaf3d99111d4ea36f543f8a
Marian Breitmeyer <info@marianbreitmeyer.de>
2024-05-13 20:41:21 +0200

    fix: content and mobile styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ import GradientStrip from './GradientStrip';
 
 const Footer = () => (
   <footer className="w-full h-auto bg-stone-800 dark:bg-stone-950 relative">
-    <div className="wrapper-no-p flex flex-row items-center justify-between text-sm py-20 md:py-24 px-6 md:px-10">
+    <div className="wrapper-no-p flex flex-col md:flex-row md:items-center justify-between text-sm py-12 md:py-24 px-6 md:px-10">
       <p className="text-stone-400 dark:text-stone-500">
-        Tiredness is a lifestyle feature.
+        As a father of three – Tiredness is a lifestyle feature.
       </p>
       <div className="flex flex-row items-center gap-x-6 md:gap-x-10 text-stone-600 dark:text-stone-600">
         <Link href="/imprint">Imprint</Link>
